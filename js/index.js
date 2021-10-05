7b9391604bc686b39549aa4caed3748f6e76d2e9
albertomatos13 <91989454+albertomatos13@users.noreply.github.com>
2021-10-05 18:22:48 -0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,57 @@
+        const $video = document.querySelector("#video")
+        const $play = document.querySelector("#play")
+        const $pause = document.querySelector("#pause")
+        const $backward = document.querySelector("#backward")
+        const $forward = document.querySelector("#forward")
+        
+        
+        $play.addEventListener("click", handleplay)
+        $pause.addEventListener("click", handlepause)
+       
+        function handleplay() {
+            $video.play()
+            $play.hidden = true
+            $pause.hidden = false
+            console.log("le diste click al boton de play")
+        }
+        function handlepause() {
+            $video.pause()
+            $pause.hidden = true
+            $play.hidden = false
+            console.log("le diste click al boton de pause")
+
+        }
+
+        $backward.addEventListener("click", handlebackward)
+        function handlebackward() {
+            $video.currentTime -=10
+            console.log(" Retroceder 10 segundos")
+        }
+
+        $forward.addEventListener("click", handleforward)
+        function handleforward() {
+            $video.currentTime +=10
+            console.log("Adelantar 10 segundos", $video.currentTime)
+        }
+
+        const $progress = document.querySelector("#progress")
+        $video.addEventListener("loadedmetadata", handleLoaded)
+        $video.addEventListener("timeupdate", handleTimeUpdate)
+
+        function handleLoaded(){
+            $progress.max = $video.duration
+            console.log("ha cargado mi video", $video.duration)
+        }
+
+        function handleTimeUpdate(){
+            $progress.value = $video.currentTime
+           // console.log("tiempo actual", $video.currentTime)
+
+        }
+
+        $progress.addEventListener("input", handleInput)
+
+        function handleInput(){
+            $video.currentTime = $progress.value
+            console.log($progress.value)
+        }
